702298912db509a96efdbdad448a709051536d1a
Rik Goldman <rikgoldman@gmail.com>
2022-10-20 11:26:38 -0400

    updates with link check

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import proselint
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import json
 from readtime import of_markdown
+import tempfile
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,6 +19,7 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload():
+
     with open("/tmp/output_file", "bw") as f:
         chunk_size = 4096
         while True:
@@ -24,6 +27,7 @@ def upload():
             if len(chunk) == 0:
                 return
             f.write(chunk)
+    
 @app.route('/', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['md_file']
@@ -32,22 +36,28 @@ def upload_file():
     
     #lint = subprocess.run(['vale', '--ext=.md', '--config="vale/.vale.ini"', source], capture_output=True, text=True)
     #print(lint.stdout)
+    temp = tempfile.NamedTemporaryFile(suffix='.md')
+    temp.write(source.encode("utf-8"))
+
+
     
     
     #lint = subprocess.run(['vale', 'config="./.vale.ini"', source], capture_output=True, text=True)
 
-
+       
     proselinted = proselint_annotate(source)
     #vale_feedback = vale_annotations(source)
     vale_feedback = vale_annotate(source)
     readability = vale_feedback[0]
     valelinted = vale_feedback[1]
     read_time = readtime_of_markdown(source)
+    link_check = markdownlinkcheck(temp.name)
+    temp.close()
     markdown_source = transform_markdown(source)
     md_with_proselint = markdown.markdown(proselinted, extensions=['md_in_html', 'mdx_truly_sane_lists', 'fenced_code', 'extra', 'toc', 'pymdownx.superfences', 'pymdownx.highlight'], output_format="html5") 
     md_with_vale = markdown.markdown(valelinted, extensions=['md_in_html', 'mdx_truly_sane_lists', 'fenced_code', 'extra', 'toc', 'pymdownx.superfences', 'pymdownx.highlight'], output_format="html5") 
-    #lintmarkdown(source)
-    return render_template('layout.html', readability=readability, proselint=md_with_proselint, read_time=read_time, vale=md_with_vale, md=markdown_source)
+    
+    return render_template('layout.html', readability=readability, proselint=md_with_proselint, read_time=read_time, vale=md_with_vale, md=markdown_source, link_check=link_check)
     #return render_template('layout.html', proselint=md_with_proselint, read_time=read_time)
     #return jsonify(valelinted)
     #uploaded_file = request.files['md_file']
@@ -61,7 +71,7 @@ def upload_file():
 
 def proselinter(input):
     errors = json.loads(proselint.tools.errors_to_json(proselint.tools.lint(input, config_file_path='./proselintconfig/config.json')))
-    print(errors)
+    #print(errors)
     return errors
 
 def proselint_annotate(source):
@@ -79,8 +89,9 @@ def proselint_annotate(source):
         #lines[int(lint['line'])-1] = lines[int(lint['line'])-1].replace(str(original), f'<span style="color:{c}" title="{m}">{original}</span>')
         
     #out = '\n'.join(lines)
+    
         lines[int(lint['line'])-1] = lines[int(lint['line'])-1].replace(str(original), f'<bold><a href="#" style="color:{c}; text-decoration: red wavy underline;" data-toggle="tooltip" title="{m}">{original}</a></bold>')
-        print(lines[int(lint['line'])-1])
+        #print(lines[int(lint['line'])-1])
     out = '\n'.join(lines)
     return out    
 
@@ -125,7 +136,9 @@ def vale_annotate(source):
         if ("Readability" in check['Check']) or ("![" in original and "(" in original):
             pass
         else:
-            lines[int(line)-1] = lines[int(line)-1].replace(original, f'{original} &nbsp; <span style="font-size:8pt" class="badge badge-{c} btn-sm" data-placement="top" data-trigger="hover" data-toggle="tooltip" title="{rule[0]}.{rule[1]}: {m} ({start}-{end})">{rule[1]}</span>')
+            #       <a type="button" class="btn" href="#" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover">Toggle popover</a>
+
+            lines[int(line)-1] = lines[int(line)-1].replace(original, f'{original} &nbsp; <a type="button" class="btn btn-sm btn-{c}" style="padding: .05rem .25rem;font-size:8pt" data-placement="bottom" data-trigger="hover" data-toggle="popover" title="{rule[0]}: {rule[1]}" data-content="{m}">{rule[1]}</a>')
     #out = '\n'.join(lines)
     #out = "No Feedback"
     #return readability, out
@@ -142,7 +155,7 @@ def vale(source):
         end = check["Span"][1]
         severity = check["Severity"]
         message = check["Message"]
-        print(f"line: {line}|start: {start}|end: {end}|severity: {severity}|message: {message}")       
+            
 
 def readtime_of_markdown(source):
     return of_markdown(source)
@@ -153,20 +166,38 @@ def transform_markdown(source):
     from pygments import highlight
     from pygments.lexers.markup import MarkdownLexer
     from pygments.formatters import HtmlFormatter
+    
+    
     lexer = MarkdownLexer()
-    formatter = HtmlFormatter(linenos=False, cssclass="source")
-    result = highlight(source, lexer, formatter)
-    print(result)
-    return result
+    formatter = HtmlFormatter(cssclass="markdown")
+    
+    return highlight(source, lexer, formatter)
 
+    
+    
     
     #source = source.replace("<", "&lt;").replace(">", "&gt;")
-    return source
+    #return source
+    
+def markdownlinkcheck(path):
+    
+    result = subprocess.run(['/home/rik/.npm-packages/bin/markdown-link-check', path], capture_output=True, text=True)
+    if result.stdout:
+        import re
+        #regex = re.compile(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", re.IGNORECASE)
+        out = []
+        for line in result.stdout:
+            url = re.match(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", line)
+            if url:
+                print(url.groups())
+            #line = line.replace(url, f'<a href="{url}">{url}</a>')
+            #out.append(line)
+        return result.stdout
+    else:
+        return "No Results"
 
-#def lintmarkdown(source):
-#    from pymarkdown import PyMarkdownLint
     
-    
+
 
 if __name__ == '__main__':
    app.run(debug = True)
